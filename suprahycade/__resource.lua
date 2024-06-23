db726a94fd947217d781e91a9d63e8b947a28722
hrissa011 <hrissasteam@gmail.com>
2024-06-23 22:04:44 +0100

    Add files via upload

---
+++ suprahycade/__resource.lua
@@ -0,0 +1,23 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+ 
+files {
+    'vehicles.meta',
+    'carvariations.meta',
+    'carcols.meta',
+    'handling.meta',
+    'vehiclelayouts.meta',    -- Not Required
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'   -- Not Required
+
+
+client_script {
+    'vehicle_names.lua'    -- Not Required
+}
+-- ┌⇨ Discord https://discord.gg/z97o 
+--  ⇨ 𝐒𝐓𝐎𝐑𝐄 𝐙𝟗𝟕𝐎 
+-- └⇨#z97o #z9xx #urmn9oor
